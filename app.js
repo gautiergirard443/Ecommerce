2fb305feb2e69a8d7d038d22e15361a479400b83
gautiergirard443 <gautiergirard444@gmail.com>
2021-05-27 15:15:29 +0200

    oui

--- app.js
+++
@@ -1,8 +0,0 @@
-import React from React 
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/gautiergirard443/Ecommerce.git
-git push -u origin main
---
+++ ecommerce/src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='CHAUSSURES'></h1>
+          <span className='subtitle'>Acheter maintenant!</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='CASQUETTES'></h1>
+          <span className='subtitle'>Acheter maintenant!</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='T-SHIRTS'></h1>
+          <span className='subtitle'>Acheter maintenant!</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='LUNETTES'></h1>
+          <span className='subtitle'>Acheter maintenant!</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='JOGGINGS'></h1>
+          <span className='subtitle'>Acheter maintenant!</span>
+        </div>
+      </div>
+    </div> 
+  </div>
+);
+
+
+export default HomePage;
