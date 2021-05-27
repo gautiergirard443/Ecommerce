b7b862f363d215459fe0ec37d5ab9863d8540aef
Gautier <gautiergirard444@gmail.com>
2021-05-27 14:29:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 import React from React 
 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gautiergirard443/Ecommerce.git
+git push -u origin main
